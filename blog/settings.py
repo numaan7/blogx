b3e7fa9e0f33e0d61115f478f01d0cfac703b8ed
numaan7 <101054796+numaan7@users.noreply.github.com>
2022-03-08 06:41:26 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_DIRS=[
+    os.path.join(BASE_DIR,"globalstaticfiles"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -137,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #this code
-STATIC_URL= "/static/"
+
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -154,12 +156,9 @@ MEDIA_URL = '/media/'
                                   
 
       
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-if DEBUG:
-
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-      STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #ckeditor upload path
