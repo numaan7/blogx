80257761e1a03b9e45ad9ec71abdb91f125ed450
numaan7 <101054796+numaan7@users.noreply.github.com>
2022-03-08 06:29:22 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include,re_path
 from django.contrib.sitemaps.views import sitemap 
 from django.conf import settings
-from django.conf.urls import re_path as url
+from django.urls import re_path as url
 
 from django.conf import settings
 
